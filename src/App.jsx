3fba3cbf17dec014f64cf7f8cbe9d2b38efc8251
shikhadogra <shikhadogra@Mac.attlocal.net>
2025-11-25 18:05:01 -0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Books from "./pages/Books";
 import BookDetails from "./pages/BookDetails";
